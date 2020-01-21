d65542b9b43df6eb89d85771e0eb2b1ed061a371
guevart <julius.hekkala@gmail.com>
2020-01-21 09:37:35 +0200

    Lab 2

--- app/src/main/java/com/juliushekkala/juliusmobilecomp/MainActivity.kt
+++ app/src/main/java/com/juliushekkala/juliusmobilecomp/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.juliushekkala.juliusmobilecomp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fab_time.setOnClickListener {
+            val intent = Intent(applicationContext, TimeActivity::class.java)
+            startActivity(intent)
+        }
+
+        fab_map.setOnClickListener {
+            val intent = Intent(applicationContext, MapActivity::class.java)
+            startActivity(intent)
+        }
+
+        var fabOpened = false
+        fab.setOnClickListener {
+            if (!fabOpened) {
+                fabOpened = true
+                fab_time.animate().translationY(-resources.getDimension(R.dimen.standard_66))
+                fab_map.animate().translationY(-resources.getDimension(R.dimen.standard_116))
+            } else {
+                fabOpened = false
+                fab_time.animate().translationY(0f)
+                fab_map.animate().translationY(0f)
+            }
+        }
+
+
+
     }
 }
