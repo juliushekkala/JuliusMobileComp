70fc104033085dfa68ef2bbf1b1aa9192dbd8db7
guevart <julius.hekkala@gmail.com>
2020-02-04 09:52:30 +0200

    Lab 4

--- app/src/main/java/com/juliushekkala/juliusmobilecomp/MainActivity.kt
+++ app/src/main/java/com/juliushekkala/juliusmobilecomp/MainActivity.kt
@@ -3,7 +3,11 @@ package com.juliushekkala.juliusmobilecomp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 
 class MainActivity : AppCompatActivity() {
@@ -35,10 +39,37 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        val data = arrayOf("Oulu", "Helsinki", "Tampere")
 
-        val reminderAdapter = ReminderAdapter(applicationContext, data)
-        list.adapter = reminderAdapter
 
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        refreshList()
+
+    }
+
+
+    private fun refreshList() {
+        doAsync {
+            val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "reminders").build()
+            val reminders = db.reminderDao().getReminders()
+            db.close()
+
+            uiThread {
+
+                if (!reminders.isEmpty()) {
+                    val adapter = ReminderAdapter(applicationContext, reminders)
+                    list.adapter = adapter
+                } else {
+                    toast("Impossible to show reminders")
+                }
+
+
+            }
+        }
     }
 }
